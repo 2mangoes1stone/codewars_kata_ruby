03418c17dc37f4d13c97c6cd7b397eb68defcbb7
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-04-06 20:12:44 +1000

    test

--- who_likes_it.rb
+++ who_likes_it.rb
@@ -8,6 +8,7 @@
 # likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
 # likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 # For more than 4 names, the number in 2 and others simply increases.
+#12132323242
 
 def likes(names)
   if names.any? == false
